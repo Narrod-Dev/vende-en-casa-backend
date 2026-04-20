b8352ffe3bb657b6d46747931f7f2f91e0186e40
Nerling Hernandez <nerlingfhg@gmail.com>
2026-04-20 14:24:35 -0600

    feat(crear-modulo): Agregar el esquema del modulo messages

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { UsersController } from './modules/users/controllers/users.controller';
+import { MessagesModule } from './modules/messages/messages.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, MessagesModule],
   controllers: [UsersController],
   providers: [],
 })
