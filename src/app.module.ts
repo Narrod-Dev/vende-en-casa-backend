5ff8af915b91d7fa627ca26a84e139e631d908ab
Nerling Hernandez <nerlingfhg@gmail.com>
2026-04-27 14:20:17 -0600

    feat(crear-modulo-categories): Crear módulo de categoríes

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { UsersModule } from './modules/users/users.module';
 import { UsersController } from './modules/users/controllers/users.controller';
 import { ProductsModule } from './modules/products/products.module';
 import { MessagesModule } from './modules/messages/messages.module';
+import { CategoriesModule } from './modules/categories/categories.module';
 
 @Module({
-  imports: [UsersModule, ProductsModule, MessagesModule],
+  imports: [UsersModule, ProductsModule, MessagesModule, CategoriesModule],
   controllers: [UsersController],
   providers: [],
 })
