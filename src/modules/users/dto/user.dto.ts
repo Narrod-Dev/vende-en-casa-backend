aca548751f411ccac7411fe93c8e68868f0c7133
massiel <besismassielhernandez@gmail.com>
2026-05-13 11:24:44 -0600

    feat: agregar propiedades de la base de datos al DTO

--- src/modules/users/dto/user.dto.ts
+++ src/modules/users/dto/user.dto.ts
@@ -1,1 +1,10 @@
-export class UserDto {}
+export class UserDto {
+    id: number;
+    full_name:string;
+    email: string;
+    password_hash: string;
+    location:string;
+    role: string;
+    is_active: boolean;
+    created_at: Date;
+}
