3de8e73e59232ca5804f11e329b8cce3d2f36f97
Stefani Garcia <stefanimatamoros6@gmail.com>
2026-05-13 13:05:57 -0600

    feat(products): Crear dto product

--- src/modules/products/dto/product.dto.ts
+++ src/modules/products/dto/product.dto.ts
@@ -1,1 +1,55 @@
-export class ProductDto {}
+import { ApiProperty, PartialType } from '@nestjs/swagger';
+import {
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsInt()
+  @IsPositive()
+  @IsNotEmpty()
+  @ApiProperty()
+  seller_id: number;
+
+  @IsInt()
+  @IsPositive()
+  @IsNotEmpty()
+  @ApiProperty()
+  category_id: number;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @ApiProperty()
+  title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(10)
+  @ApiProperty()
+  description: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @ApiProperty()
+  condition: string;
+
+  @IsNumber()
+  @IsPositive()
+  @IsNotEmpty()
+  @ApiProperty()
+  price: number;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @ApiProperty()
+  status: string;
+
+}
+export class UpdateProductDto extends PartialType(CreateProductDto) {}
