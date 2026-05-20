d073c8f386da3074d0d35955561abc7ac0599914
Stefani Garcia <stefanimatamoros6@gmail.com>
2026-05-20 16:49:22 -0600

    feat: agregar los servicios al modulo de ratings

--- src/modules/ratings/services/ratings.service.ts
+++ src/modules/ratings/services/ratings.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Rating } from '../entities/rating.entity';
+import { CreateRatingDto, UpdateRatingDto } from '../dto/rating.dto';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class RatingsService {}
+export class RatingsService {
+        constructor(
+        @InjectRepository(Rating)
+        private readonly ratingRepository: Repository<Rating>,
+    ) {}
+
+    async create(createRatingDto: CreateRatingDto): Promise<Rating> {
+        const rating = this.ratingRepository.create(createRatingDto);
+        return await this.ratingRepository.save(rating);
+    }
+
+    async findAll(): Promise<Rating[]> {
+        return await this.ratingRepository.find();
+    }
+
+    async findOne(id: number): Promise<Rating> {
+        const rating = await this.ratingRepository.findOneBy({ id });
+
+        if (!rating) {
+            throw new NotFoundException(`Rating con id ${id} no encontrado.`);
+        }
+
+        return rating;
+    }
+
+    async update(id: number, updateRatingDto: UpdateRatingDto): Promise<Rating> {
+        const rating = await this.findOne(id);
+        Object.assign(rating, updateRatingDto);
+        return await this.ratingRepository.save(rating);
+    }
+
+    async remove(id: number): Promise<void> {
+        const rating = await this.findOne(id);
+        await this.ratingRepository.remove(rating);
+    }
+}
