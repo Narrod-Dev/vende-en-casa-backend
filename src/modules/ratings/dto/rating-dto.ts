d02358cffa23b94393b153bd26c2068c6cac7873
massiel <besismassielhernandez@gmail.com>
2026-05-20 11:19:46 -0600

    feat: reestructura rating dto con validaciones

--- src/modules/ratings/dto/rating-dto.ts
+++ src/modules/ratings/dto/rating-dto.ts
@@ -1,1 +1,47 @@
-export class CreateRatingDto{}
+import { ApiProperty } from "@nestjs/swagger";
+import { 
+    IsInt, 
+    IsNotEmpty, 
+    IsOptional, 
+    IsPositive, 
+    IsString } 
+    from "class-validator";
+
+
+
+export class RatingDto{
+    @IsInt()
+    @IsPositive()
+    @IsOptional()
+    @ApiProperty()
+    reviewer_id: number;
+
+    @IsInt()
+    @IsPositive()
+    @IsOptional()
+    @ApiProperty()
+    reviewee_id: number;
+    
+    @IsInt()
+    @IsPositive()
+    @IsOptional()
+    @ApiProperty()
+    product_id: number;
+
+    @IsInt()
+    @IsPositive()
+    @IsNotEmpty()
+    @ApiProperty()
+    score: number;
+
+    @IsString()
+    @IsOptional()
+    @ApiProperty()
+    comment: string;
+
+    @IsString()
+    @IsOptional()
+    @ApiProperty()
+    created_at: string;
+
+}
