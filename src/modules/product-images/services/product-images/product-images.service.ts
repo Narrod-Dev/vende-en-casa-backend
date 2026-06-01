33a312ac39663a9efe19dd4cc35d9f30c420c464
Stefani Garcia <stefanimatamoros6@gmail.com>
2026-06-01 01:38:04 -0600

    Agregar validaciones de productos y control de imágenes

--- src/modules/product-images/services/product-images/product-images.service.ts
+++ src/modules/product-images/services/product-images/product-images.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, NotFoundException, BadRequestException, HttpException } from '@nestjs/common';
 import { ProductImage } from '../../entities/product-image.entity';
 import { Repository } from 'typeorm';
 import { CreateProductImageDto, UpdateProductImageDto } from '../../dto/product-image.dto';
@@ -13,18 +13,43 @@ export class ProductImagesService {
 
   async create(createProductImageDto: CreateProductImageDto) {
     try {
+// Verifica cuantas imagenes tiene actualmente el producto
+      const totalImages = await this.productImageRepository.count({
+        where: {
+          product_id: createProductImageDto.product_id,
+        },
+    });
+// Impide agregar más de 3 imagenes por producto
+      if (totalImages >= 3) {
+        throw new BadRequestException(
+          'El producto ya tiene el maximo de imagenes permitidas',
+        );
+      }
+//control de portada
+// Asegura que solo una imagen por producto pueda ser portada (is_main = true)
+// la imagen seleccionda se establece como true y el resto pasa a ser false
+    if (createProductImageDto.is_main) {
+      await this.productImageRepository.update(
+        { product_id: createProductImageDto.product_id },
+        { is_main: false },
+      );
+    }
+
       const productImage = this.productImageRepository.create(
         createProductImageDto,
       );
-
       await this.productImageRepository.save(productImage);
 
       return productImage;
     } catch (error) {
       console.log(error);
+// esto permite mantener el mensaje de error por poner más imagenes de las permitidas
+      if (error instanceof HttpException) {
+        throw error;
+      }
 
       throw new InternalServerErrorException(
-        'Error al crear la imagen del producto',
+        'Error al agregar imagen del producto',
       );
     }
   }
